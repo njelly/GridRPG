ebb4033f80a05768cd73e861d4eab7977cc9f830
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2021-05-21 21:10:45 -0600

    upgraded unity to 2020.3.9f1, added GameContext

---
+++ Assets/_Game/Scripts/Game/ActorComponents/Interactor.cs
@@ -0,0 +1,30 @@
+﻿namespace Tofunaut.GridRPG.Game.ActorComponents
+{
+    public class Interactor : ActorComponent
+    {
+        private bool _wasInteracting;
+        
+        private void Update()
+        {
+            var isInteracting = Actor.Input.Interacting;
+            if (!_wasInteracting && isInteracting)
+            {
+                BeginInteracting();
+            }
+            else if(_wasInteracting && !isInteracting)
+            {
+                EndInteracting();
+            }
+        }
+
+        private void BeginInteracting()
+        {
+            _wasInteracting = true;
+        }
+
+        private void EndInteracting()
+        {
+            _wasInteracting = false;
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Game/GameContext.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.GridRPG.Game
+{
+    public class GameContext : MonoBehaviour
+    {
+        private static GameContext _instance;
+
+        public static MapManager MapManager => _instance._mapManager;
+
+        [SerializeField] private MapManager _mapManager;
+
+        public void Awake()
+        {
+            if (_instance)
+            {
+                Debug.LogError("an instance of GameContext already exists");
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this;
+        }
+    }
+}
